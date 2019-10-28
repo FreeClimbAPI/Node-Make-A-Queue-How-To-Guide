2aebe1a96370f425edbe76ff4463b0276ae44ab2
Francis San Filippo <fsanfilipp@vailsys.com>
2019-10-28 11:03:16 -0500

    freeclimb code changes

--- makeAQueue.js
+++ makeAQueue.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
-const persephonySDK = require('@persephony/sdk')
+const freeclimbSDK = require('@freeclimb/sdk')
 const accountId = process.env.ACCOUNT_ID
 const authToken = process.env.AUTH_TOKEN
-const persephony = persephonySDK(accountId, authToken)
+const freeclimb = freeclimbSDK(accountId, authToken)
 
 const options = {
   alias: 'Tutorial Queue',
   maxSize: 25
 }
 //Invoke method to create a queue with the options provided
-persephony.api.queues.create(options).then(queue => {
+freeclimb.api.queues.create(options).then(queue => {
   // use created queue
   const queueId = queue.queueId
-  persephony.api.queues.get(queueId).then(queue => {
+  freeclimb.api.queues.get(queueId).then(queue => {
     console.log(queue)
   })
 }).catch(err => {
