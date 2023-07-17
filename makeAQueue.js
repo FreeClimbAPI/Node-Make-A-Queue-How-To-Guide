5d2d49cede5eb429bc6eaf12d3d7b636b5b4327a
Kate Brune <kbrune@vailsys.com>
2023-07-17 14:53:11 -0500

    update code

--- makeAQueue.js
+++ makeAQueue.js
@@ -1,20 +1,17 @@
 require('dotenv').config()
 const freeclimbSDK = require('@freeclimb/sdk')
+
 const accountId = process.env.ACCOUNT_ID
 const apiKey = process.env.API_KEY
-const freeclimb = freeclimbSDK(accountId, apiKey)
+const freeclimbConfig = new freeclimbSDK.createConfiguration({ accountId, apiKey })
+const apiInstance = new freeclimbSDK.DefaultApi(freeclimbConfig)
 
-const options = {
+//Invoke method to create a queue with the options provided
+apiInstance.createAQueue({
   alias: 'Tutorial Queue',
   maxSize: 25
-}
-//Invoke method to create a queue with the options provided
-freeclimb.api.queues.create(options).then(queue => {
-  // use created queue
-  const queueId = queue.queueId
-  freeclimb.api.queues.get(queueId).then(queue => {
-    console.log(queue)
-  })
-}).catch(err => {
-  // Catch Errors
-})
+}).then(({ queueId }) => {
+  // Get the queue and log details
+  apiInstance.getAQueue(queueId).then((queue) => console.log(queue))
+}).catch(console.log)
+
